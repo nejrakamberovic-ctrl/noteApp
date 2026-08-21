cf4e6635193480076fac5d37285535faffd52219
Nejra <nejra.kamberovic@gmail.com>
2026-08-21 19:02:58 +0200

    poruka

--- func.py
+++ func.py
@@ -1,6 +1,9 @@
 def zapisi(file_name, tekst):
-    with open(f"{file_name}.txt", "w", encoding="utf-8") as file:
-        file.write(tekst)
+    try:
+        with open(f"{file_name}.txt", "w", encoding="utf-8") as file:
+            file.write(tekst)
+    except Exception as e:
+        print("greska", e)
 
 def procitaj():
     pass
